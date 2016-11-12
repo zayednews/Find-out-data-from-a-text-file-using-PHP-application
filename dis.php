d39e5bec4d1a47448c737ad4f61ed5455028277f
Zayed Bin Maleque (News) <ne.zayed@gmail.com>
2016-11-12 17:21:19 +0600

    Update dis.php

--- dis.php
+++ dis.php
@@ -2,17 +2,15 @@
 <?php
 
 $d=$_POST['area'];
-//echo $d;
 
 
 $file = fopen("locs.txt", "r") or die("Unable to open file!");
 
 while(!feof($file)) {
 	$line=fgets($file);
-	//echo $line;
+	
 	$var=explode(":", $line);
-	//echo $var[0];
-	//echo $d;
+	
 	if(trim($var[0])==trim($d)){
 	echo trim($var[1]);
 	echo "<br>";
